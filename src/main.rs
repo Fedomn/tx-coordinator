563ecb8c1f6dd0df00a2e4b51df4ac8bd58dbfe3
Fedomn <fedomn.ma@gmail.com>
2022-01-27 00:23:13 +0800

    init cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(name = "Tx-Coordinator", author = "fedomn", version = "1.0.0")]
+pub struct Args {
+    #[clap(help = "The path of configuration file", default_value = "./cfg.toml")]
+    cfg: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Got args: {:?}", args);
 }
