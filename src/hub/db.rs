bc0080c72bb00cdad9bab5c39a2a38fa77d5a00a
Fedomn <fedomn.ma@gmail.com>
2022-01-27 21:22:44 +0800

    add db conn pool

--- src/hub/db.rs
+++ src/hub/db.rs
@@ -1,5 +1,46 @@
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{Pool, Postgres};
+
 pub struct DB {
     pub schema: String,
     pub secret: String,
     pub sql_files: Vec<String>,
+}
+
+impl DB {
+    pub async fn gen_pool(&self) -> Pool<Postgres> {
+        PgPoolOptions::new()
+            .max_connections(5)
+            .connect(self.secret.as_str())
+            .await
+            .unwrap()
+    }
+}
+
+#[cfg(test)]
+mod db_test {
+    use anyhow::Result;
+
+    use super::*;
+
+    #[tokio::test]
+    #[ignore]
+    async fn connect_local_db_works() -> Result<()> {
+        let db = DB {
+            schema: "db1".to_string(),
+            secret: "postgres://postgres:@localhost/db1".to_string(),
+            sql_files: vec![],
+        };
+
+        let pool = db.gen_pool().await;
+
+        let row: (i64,) = sqlx::query_as("SELECT $1")
+            .bind(150_i64)
+            .fetch_one(&pool)
+            .await?;
+
+        assert_eq!(row.0, 150);
+
+        Ok(())
+    }
 }
